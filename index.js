03f24adf8e73f4a6a54e3978f38b7843c28b45fd
diegoson <110030270+D3centX@users.noreply.github.com>
2024-12-24 08:01:31 +0200

    Delete

--- index.js
+++ index.js
@@ -1,31 +1,35 @@
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
-const pino = require('pino');
-const NodeCache = require('node-cache');
-const { Mutex } = require('async-mutex');
-const crypto = require('crypto');
+const express = require("express");
+const fs = require("fs");
+
+const pino = require("pino");
+const NodeCache = require("node-cache");
+const { Mutex } = require("async-mutex");
+const PastebinAPI = require("pastebin-js");
+const path = require("path");
+pastebin = new PastebinAPI("EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL");
+const crypto = require("crypto");
+const axios = require("axios");
 const {
     default: makeWASocket,
     useMultiFileAuthState,
     delay,
     Browsers,
     makeCacheableSignalKeyStore,
-    DisconnectReason
-} = require('@whiskeysockets/baileys');
-const { saveSession, getSession, deleteSession } = require('./mongo');
+    DisconnectReason,
+} = require("@whiskeysockets/baileys");
+const { saveSession, getSession, deleteSession } = require("./mongo");
 const app = express();
 const port = 3000;
 let session;
 const msgRetryCounterCache = new NodeCache();
 const mutex = new Mutex();
-app.use(express.static(path.join(__dirname, 'pages')));
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'pages', 'dashboard.html'));
+app.use(express.static(path.join(__dirname, "pages")));
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "pages", "dashboard.html"));
 });
 async function connector(Num, res) {
-    const sessionId = `Naxor~${crypto.randomBytes(8).toString('hex')}`;
-    const sessionDir = './session';
+    const sessionId = `Naxor~${crypto.randomBytes(8).toString("hex")}`;
+    const sessionDir = "./session";
     if (!fs.existsSync(sessionDir)) {
         fs.mkdirSync(sessionDir);
     }
@@ -38,65 +42,87 @@ async function connector(Num, res) {
     session = makeWASocket({
         auth: {
             creds: state.creds,
-            keys: makeCacheableSignalKeyStore(state.keys, pino({ level: 'fatal' }).child({ level: 'fatal' }))
+            keys: makeCacheableSignalKeyStore(
+                state.keys,
+                pino({ level: "fatal" }).child({ level: "fatal" }),
+            ),
         },
         printQRInTerminal: false,
-        logger: pino({ level: 'fatal' }).child({ level: 'fatal' }),
+        logger: pino({ level: "fatal" }).child({ level: "fatal" }),
         browser: Browsers.macOS("Safari"),
         markOnlineOnConnect: true,
-        msgRetryCounterCache
+        msgRetryCounterCache,
     });
     if (!session.authState.creds.registered) {
         await delay(1500);
-        Num = Num.replace(/[^0-9]/g, '');
+        Num = Num.replace(/[^0-9]/g, "");
         const code = await session.requestPairingCode(Num);
         if (!res.headersSent) {
-            res.send({ code: code?.match(/.{1,4}/g)?.join('-') });
+            res.send({ code: code?.match(/.{1,4}/g)?.join("-") });
         }
     }
-    session.ev.on('creds.update', async () => {
+    session.ev.on("creds.update", async () => {
         await saveCreds();
-        await saveSession(sessionId, state); 
+        await saveSession(sessionId, state);
     });
-    session.ev.on('connection.update', async (update) => {
+    session.ev.on("connection.update", async (update) => {
         const { connection, lastDisconnect } = update;
-        if (connection === 'open') {
-            console.log('Connected successfully');
+        if (connection === "open") {
+            console.log("Connected successfully");
             await delay(5000);
-            console.log(state.creds)
-            await delay(800)
-          const output = await axios.post('http://paste.c-net.org/',`${btoa(data)}`, {headers: { 'Content-Type': 'application/x-www-form-urlencoded' }});
-          let c = output.data.split('/')[3]
-           await session.sendMessage(session.user.id, {text: 'Secktor;;;'+c});	
-            console.log('[Session] Session online');
-        } else if (connection === 'close') {
+
+            pastebin
+                .createPasteFromFile(
+                    __dirname + "/session/creds.json",
+                    "SamPandey001",
+                    null,
+                    1,
+                    "N",
+                )
+                .then(async (data) => {
+                    let unique = data.split("/")[3];
+                    let Session = Buffer.from(unique).toString("base64");
+                    await session.sendMessage(session.user.id, {
+                        text: "Secktor;;;" + Session,
+                    });
+                    console.log("[Session] Session online");
+                });
+        } else if (connection === "close") {
             const reason = lastDisconnect?.error?.output?.statusCode;
             console.log(`Connection closed. Reason: ${reason}`);
-            await deleteSession(sessionId); 
+            await deleteSession(sessionId);
             reconn(reason);
         }
     });
 }
 
 function reconn(reason) {
-    if ([DisconnectReason.connectionLost, DisconnectReason.connectionClosed, DisconnectReason.restartRequired].includes(reason)) {
-        console.log('Connection lost, reconnecting...');
+    if (
+        [
+            DisconnectReason.connectionLost,
+            DisconnectReason.connectionClosed,
+            DisconnectReason.restartRequired,
+        ].includes(reason)
+    ) {
+        console.log("Connection lost, reconnecting...");
         connector();
     } else {
         console.log(`Disconnected! Reason: ${reason}`);
         session.end();
-    }}
+    }
+}
 
-app.get('/pair', async (req, res) => {
+app.get("/pair", async (req, res) => {
     const Num = req.query.code;
     if (!Num) {
-    return res.status(418).json({ message: 'Phone number is required' }); }
+        return res.status(418).json({ message: "Phone number is required" });
+    }
     const release = await mutex.acquire();
     try {
         await connector(Num, res);
     } catch (error) {
         console.error(error);
-        res.status(500).json({ error: 'server Error' });
+        res.status(500).json({ error: "server Error" });
     } finally {
         release();
     }
